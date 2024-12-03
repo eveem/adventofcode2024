d8f008407c954e1e1640c35c9f810aa91c9795a6
Monpriya <monpriya.t@ibmdt.co.th>
2024-12-03 15:23:31 +0700

    finish day3 second start ⭐

--- day_03/main.py
+++ day_03/main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 def read_input(filename):
     lines = []
     with open(filename, "r") as file:
@@ -26,7 +28,7 @@ def extract_valid_mul(s):
                 try:
                     t = t.replace("(", "").replace(")", "")
                     x, y = map(int, t.split(","))
-                    stack.append([x, y])
+                    stack.append([x, y, st])
                 except:
                     pass
             i = st
@@ -34,20 +36,55 @@ def extract_valid_mul(s):
     
     return stack
 
+def find_do_dont(s):
+    i = 0
+    n = len(s)
+    res = defaultdict(bool)
+    while i < n:
+        if s[i:i + 7] == "don't()":
+            res[i] = False
+        if s[i:i + 4] == "do()":
+            res[i] = True
+        i += 1
+        
+    return res
+
+def format_pairs(pairs):
+    formatted = defaultdict(list)
+    for x, y, idx in pairs:
+        formatted[idx] = [x, y]
+    return formatted
+
 def first_star():
     total = 0
-    # lines = read_input("1_small_input.txt")
-    lines = read_input("input.txt")
+    lines = read_input("1_small_input.txt")
+    # lines = read_input("input.txt")
 
     for line in lines:
         pairs = extract_valid_mul(line)
-        for x, y in pairs:
+        for x, y, _ in pairs:
             total += x * y
 
     print(f"first star result: {total}")
 
 def second_star():
     total = 0
+    # lines = read_input("2_small_input.txt")
+    lines = read_input("input.txt")
+    enabled = True
+    for line in lines:
+        pairs = extract_valid_mul(line)
+        formatted_pairs = format_pairs(pairs)
+        do_dont = find_do_dont(line)
+
+        n = len(line)
+        for i in range(n):
+            if i in do_dont:
+                enabled = do_dont[i]
+            
+            if i in formatted_pairs and enabled:
+                total += formatted_pairs[i][0] * formatted_pairs[i][1]
+
     print(f"second star result: {total}")
 
 if __name__ == "__main__":
--- day_03/test_main.py
+++ day_03/test_main.py
@@ -1,5 +1,5 @@
 import unittest
-from main import read_input, extract_valid_mul
+from main import read_input, extract_valid_mul, find_do_dont, format_pairs
 
 class TestMain(unittest.TestCase):
     def test_read_small_input(self):
@@ -22,7 +22,7 @@ class TestMain(unittest.TestCase):
     
     def test_extract_valid_mul_simple(self):
         data = extract_valid_mul("xmul(2,4)%mul[3,7]^not_mul(5,5)")
-        self.assertEqual(data, [[5, 5], [2, 4]])
+        self.assertEqual(data, [[5, 5, 23], [2, 4, 1]])
 
     def test_extract_valid_mul_no_valid(self):
         data = extract_valid_mul("xmul(2,4]")
@@ -30,4 +30,12 @@ class TestMain(unittest.TestCase):
 
     def test_extract_valid_mul_complex(self):
         data = extract_valid_mul("mul(32,64]then(mul(11,8)mul(8,5))")
-        self.assertEqual(data, [[8, 5], [11, 8]])
+        self.assertEqual(data, [[8, 5, 24], [11, 8, 15]])
+
+    def test_find_do_dont_with_index(self):
+        data = find_do_dont("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+        self.assertEqual(data, {20: False, 59: True})
+
+    def test_format_pairs(self):
+        data = [[8, 5, 24], [11, 8, 15]]
+        self.assertEqual(format_pairs(data), {24: [8, 5], 15: [11, 8]})
