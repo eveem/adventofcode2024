a1a0ee20930300460a46b36430e0069af556b9d6
Monpriya <monpriya.t@ibmdt.co.th>
2024-12-03 13:07:06 +0700

    finish first half

---
+++ day_03/main.py
@@ -0,0 +1,55 @@
+def read_input(filename):
+    lines = []
+    with open(filename, "r") as file:
+        for line in file:
+            lines.append(line)
+    return lines
+
+def extract_valid_mul(s):
+    stack = []
+    n = len(s)
+    i = n - 1
+
+    while i >= 2:
+        if s[i - 2:i + 1] == "mul":
+            st = i - 2
+            if s[i + 1] == "(":
+                t = s[i + 1]
+                i += 2
+                while i < n and s[i] != ")":
+                    t += s[i]
+                    i += 1
+                
+                if i < n:
+                    t += s[i]
+                
+                try:
+                    t = t.replace("(", "").replace(")", "")
+                    x, y = map(int, t.split(","))
+                    stack.append([x, y])
+                except:
+                    pass
+            i = st
+        i -= 1
+    
+    return stack
+
+def first_star():
+    total = 0
+    # lines = read_input("small_input.txt")
+    lines = read_input("input.txt")
+
+    for line in lines:
+        pairs = extract_valid_mul(line)
+        for x, y in pairs:
+            total += x * y
+
+    print(f"first star result: {total}")
+
+def second_star():
+    total = 0
+    print(f"second star result: {total}")
+
+if __name__ == "__main__":
+    first_star()
+    second_star()
---
+++ day_03/test_main.py
@@ -0,0 +1,27 @@
+import unittest
+from main import read_input, extract_valid_mul
+
+class TestMain(unittest.TestCase):
+    def test_read_small_input(self):
+        filename = "small_input.txt"
+        lines = read_input(filename)
+
+        self.assertEqual(len(lines), 1)
+
+    def test_read_input(self):
+        filename = "input.txt"
+        lines = read_input(filename)
+
+        self.assertEqual(len(lines), 6)
+    
+    def test_extract_valid_mul_simple(self):
+        data = extract_valid_mul("xmul(2,4)%mul[3,7]^not_mul(5,5)")
+        self.assertEqual(data, [[2, 4], [5, 5]])
+
+    def test_extract_valid_mul_no_valid(self):
+        data = extract_valid_mul("xmul(2,4]")
+        self.assertEqual(data, [])
+
+    def test_extract_valid_mul_complex(self):
+        data = extract_valid_mul("mul(32,64]then(mul(11,8)mul(8,5))")
+        self.assertEqual(data, [[11, 8], [8, 5]])
