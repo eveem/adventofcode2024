3aa6f2a76dc619a02823225d82754c1e3b056c83
Monpriya <monpriya.t@ibmdt.co.th>
2024-12-03 13:15:04 +0700

    update test

--- day_03/test_main.py
+++ day_03/test_main.py
@@ -7,6 +7,12 @@ class TestMain(unittest.TestCase):
         lines = read_input(filename)
 
         self.assertEqual(len(lines), 1)
+    
+    def test_read_small_input_single_quote(self):
+        filename = "2_small_input.txt"
+        lines = read_input(filename)
+
+        self.assertEqual(len(lines), 1)
 
     def test_read_input(self):
         filename = "input.txt"
@@ -16,7 +22,7 @@ class TestMain(unittest.TestCase):
     
     def test_extract_valid_mul_simple(self):
         data = extract_valid_mul("xmul(2,4)%mul[3,7]^not_mul(5,5)")
-        self.assertEqual(data, [[2, 4], [5, 5]])
+        self.assertEqual(data, [[5, 5], [2, 4]])
 
     def test_extract_valid_mul_no_valid(self):
         data = extract_valid_mul("xmul(2,4]")
@@ -24,4 +30,4 @@ class TestMain(unittest.TestCase):
 
     def test_extract_valid_mul_complex(self):
         data = extract_valid_mul("mul(32,64]then(mul(11,8)mul(8,5))")
-        self.assertEqual(data, [[11, 8], [8, 5]])
+        self.assertEqual(data, [[8, 5], [11, 8]])
