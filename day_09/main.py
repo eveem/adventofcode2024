91de2f367e4ed3159fd40c7b225f2083520f7fc2
Monpriya <monpriya.t@ibmdt.co.th>
2024-12-10 01:32:01 +0700

    finish day 9 second start very very brute force naaaa

--- day_09/main.py
+++ day_09/main.py
@@ -1,3 +1,5 @@
+import heapq
+
 def read_input(filename):
     with open(filename, "r") as file:
         for line in file:
@@ -37,12 +39,62 @@ def checksum(disk):
     n = len(disk)
 
     for i in range(n):
-        if disk[i] == ".":
-            break
-        total += int(disk[i]) * i
+        if disk[i] != ".":
+            total += int(disk[i]) * i
 
     return total
     
+def list_space(disk):
+    n = len(disk)
+    curr = 0
+    res = []
+    
+    for i in range(n):
+        if disk[i] == ".":
+            curr += 1
+        elif disk[i] != "." and curr != 0:
+            heapq.heappush(res, [i - curr, curr])
+            curr = 0
+    
+    return res
+
+def move_file(spaces, file, disk):
+    key, value, index = file
+    temp = []
+    
+    while spaces:
+        idx, space = heapq.heappop(spaces)
+        if space >= value and idx <= index:
+            for i in range(idx, idx + value):
+                disk[i] = key
+            for i in range(index, index + value):
+                disk[i] = "."
+            space -= value
+            if space != 0:
+                heapq.heappush(temp, [idx + value, space])
+            break
+        
+        heapq.heappush(temp, [idx, space])
+
+    return disk
+
+def list_file(disk):
+    res = []
+    n = len(disk)
+    i = 0
+    
+    while i < n:
+        ch = disk[i]
+        curr = 1
+        while i + 1 < n and disk[i] == disk[i + 1]:
+            curr += 1
+            i += 1
+        if ch != ".":
+            res.append([ch, curr, i - curr + 1])
+        i += 1
+
+    return res
+
 def first_star():
     s = read_input("small_input.txt")
     # s = read_input("input.txt")
@@ -54,11 +106,17 @@ def first_star():
     print(f"first star result: {total}")
 
 def second_star():
-    s = read_input("small_input.txt")
-    # s = read_input("input.txt")
-    
-    total = 0
+    # s = read_input("small_input.txt")
+    s = read_input("input.txt")
 
+    disk = init_disk(s)
+    files = list_file(disk)
+
+    for file in files[::-1]:
+        spaces = list_space(disk)
+        disk = move_file(spaces, file, disk)
+        
+    total = checksum(disk)
     print(f"second star result: {total}")
 
 if __name__ == "__main__":
--- day_09/test_main.py
+++ day_09/test_main.py
@@ -1,5 +1,5 @@
 import unittest
-from main import read_input, init_disk, fragment, checksum
+from main import read_input, init_disk, fragment, checksum, list_space, list_file, move_file
 
 class TestMain(unittest.TestCase):
     def test_read_small_input(self):
@@ -27,3 +27,25 @@ class TestMain(unittest.TestCase):
         disk = list("0099811188827773336446555566..............")
         expected = 1928
         self.assertEqual(checksum(disk), expected)
+    
+    def test_list_space(self):
+        disk = list("00...111...2...333.44.5555.6666.777.888899")
+        spaces = list_space(disk)
+        
+        self.assertTrue([2, 3] in spaces) # [index, space]
+        self.assertTrue([8, 3] in spaces)
+    
+    def test_list_file(self):
+        disk = list("00...111...2...333.44.5555.6666.777.888899")
+        files = list_file(disk)
+        self.assertTrue(["0", 2, 0] in files) # [key, total]
+        self.assertTrue(["8", 4, 36] in files)
+    
+    def test_move_file(self):
+        spaces = [[2, 3], [8, 3]]
+        files = ["9", 2, 40]
+        disk = list("00...111...2...333.44.5555.6666.777.888899")
+
+        updated_disk = move_file(spaces, files, disk)
+        self.assertEqual(updated_disk, list("0099.111...2...333.44.5555.6666.777.8888.."))
+
