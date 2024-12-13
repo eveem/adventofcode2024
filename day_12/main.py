2297fa3826baa2427cb9fddc9eb3b8987c922dab
Monpriya <monpriya.t@ibmdt.co.th>
2024-12-13 17:16:38 +0700

    add solution day12 part 2

--- day_12/main.py
+++ day_12/main.py
@@ -1,3 +1,5 @@
+from collections import Counter, defaultdict
+
 def read_input(filename):
     grid = []
     with open(filename, "r") as file:
@@ -33,6 +35,90 @@ def find_perimeter(grid, crop, x, y, seen):
     _helper(x, y)
     return area, perimeter, seen
 
+# def count_points(grid, crop, x, y):
+#     rows = len(grid)
+#     cols = len(grid[0])
+#     cnt = defaultdict(int)
+#     seen = set()
+#     directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+#     def _helper(x, y):
+#         seen.add((x, y))
+#         for dx, dy in directions:
+#             nx, ny = dx + x, dy + y
+#             if 0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == crop and (nx, ny) not in seen:
+#                 _helper(nx, ny)
+    
+#     _helper(x, y)
+
+#     for x, y in seen:
+#         for dx, dy in directions:
+#             nx, ny = dx + x, dy + y
+#             if nx < 0 or nx == rows or ny < 0 or ny == cols or grid[nx][ny] != crop:
+#                 cnt[(nx, ny)] += 1
+
+#     return cnt
+
+# def group_points(cnt):
+#     res = []
+
+#     def _dfs(x, y, path, directions):
+#         path.append((x, y))
+#         cnt[(x, y)] -= 1
+#         for dx, dy in directions:
+#             nx, ny = dx + x, dy + y
+#             if (nx, ny) in cnt and cnt[(nx, ny)] > 0 and (nx, ny) not in path:
+#                 _dfs(nx, ny, path, directions)
+#         return path
+
+#     temp = sorted(cnt.items(), key=lambda item: (item[1], -item[0][0], -item[0][1]), reverse=True)
+
+#     for (u, v), val in temp:
+#         while cnt[(u, v)] > 0 and (((u + 1, v) in cnt and cnt[(u + 1, v)] > 0) or ((u, v + 1) in cnt and cnt[(u, v + 1)] > 0)):
+#             if cnt[(u, v)] > 0 and (u + 1, v) in cnt and cnt[(u + 1, v)] > 0:
+#                 res.append(_dfs(u, v, [], [[1, 0]]))
+#             if cnt[(u, v)] > 0 and (u, v + 1) in cnt and cnt[(u, v + 1)] > 0:
+#                 res.append(_dfs(u, v, [], [[0, 1]]))
+
+#     for (u, v), val in cnt.items():
+#         for _ in range(val):
+#             res.append([(u, v)])
+
+#     return res
+
+def flood_field(grid, crop, x, y, visited):
+    rows = len(grid)
+    cols = len(grid[0])
+
+    directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    
+    def _helper(x, y):
+        visited.add((x, y))
+
+        for dx, dy in directions:
+            nx, ny = dx + x, dy + y
+            if 0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == crop and (nx, ny) not in visited:
+                _helper(nx, ny)
+    
+    _helper(x, y)
+    return visited
+
+
+def find_corners(grid, points):
+    corners = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+    
+    for x, y in points:
+        for dx, dy in directions:
+            nx, ny = dx + x, dy + y
+            if (nx, ny) not in points:
+                if (x - dy, y + dx) not in points or (nx - dy, ny + dx) in points:
+                    corners += 1
+
+    return corners
+
 def first_star():
     # grid = read_input("small_input_01.txt")
     # grid = read_input("small_input_02.txt")
@@ -53,10 +139,27 @@ def first_star():
     print(f"first star result: {total}")
 
 def second_star():
+    # grid = read_input("small_input_01.txt")
+    # grid = read_input("small_input_02.txt")
+    # grid = read_input("small_input_03.txt")
     # grid = read_input("small_input_04.txt")
-    grid = read_input("input.txt")
+    grid = read_input("small_input_05.txt")
+    # grid = read_input("small_input_06.txt")
+    # grid = read_input("input.txt")
     
     total = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    seen = set()
+
+    for r in range(rows):
+        for c in range(cols):
+            if (r, c) not in seen:
+                area, _, seen = find_perimeter(grid, grid[r][c], r, c, seen)
+                points = flood_field(grid, grid[r][c], r, c, set())
+                seen = seen.union(points)
+                side = find_corners(grid, points)
+                total += area * side
 
     print(f"second star result: {total}")
 
--- day_12/test_main.py
+++ day_12/test_main.py
@@ -1,5 +1,6 @@
 import unittest
-from main import read_input, find_perimeter
+from collections import Counter
+from main import read_input, find_perimeter, find_corners, flood_field
 
 class TestMain(unittest.TestCase):
     def test_read_small_input(self):
@@ -9,7 +10,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(len(grid), 4)
         self.assertEqual(grid[0], "AAAA")
     
-    def test_dfs_small_input_1(self):
+    def test_find_perimeter_small_input_1(self):
         grid = read_input("small_input_01.txt")
         expected_area = 4
         expected_perimeter = 10
@@ -21,7 +22,7 @@ class TestMain(unittest.TestCase):
         self.assertEqual(result_perimeter, expected_perimeter)
         self.assertEqual(result_seen, expected_seen)
     
-    def test_dfs_small_input_2(self):
+    def test_find_perimeter_small_input_2(self):
         grid = read_input("small_input_02.txt")
         expected_area_x = 1
         expected_perimeter_x = 4
@@ -45,3 +46,92 @@ class TestMain(unittest.TestCase):
         self.assertEqual(result_area_o, expected_area_o)
         self.assertEqual(result_perimeter_o, expected_perimeter_o)
         self.assertSetEqual(result_seen_o, expected_seen_o)
+    
+    # def test_count_points(self):
+    #     grid = [
+    #         "XXXXXX",
+    #         "XXEEEX",
+    #         "XXEXXX",
+    #         "XXEEEX",
+    #         "XXEXXX",
+    #         "XXEEEX",
+    #         "XXXXXX"
+    #     ]
+
+    #     points = [
+    #         (0, 2), (0, 3), (0, 4), (1, 5),
+    #         (2, 3), (2, 4), (2, 3), (2, 3), 
+    #         (2, 4), (3, 5), (4, 3), (4, 4),
+    #         (4, 3), (4, 3), (4, 4), (5, 5), 
+    #         (6, 2), (6, 3), (6, 4), (1, 1), 
+    #         (2, 1), (3, 1), (4, 1), (5, 1)
+    #     ]
+
+    #     expected = Counter(points)
+    #     self.assertDictEqual(count_points(grid, "E", 1, 2), expected)
+    
+    # def test_group_points(self):
+    #     points = {
+    #         (1, 1): 1, (0, 2): 1, (3, 5): 1, (4, 4): 2,
+    #         (2, 4): 2, (5, 5): 1, (6, 4): 1, (1, 5): 1,
+    #         (0, 4): 1, (4, 3): 3, (4, 1): 1, (2, 3): 3,
+    #         (2, 1): 1, (6, 3): 1, (3, 1): 1, (0, 3): 1,
+    #         (5, 1): 1, (6, 2): 1
+    #     }
+
+    #     expected = [
+    #         [(0, 2), (0, 3), (0, 4)],
+    #         [(1, 1), (2, 1), (3, 1), (4, 1), (5, 1)],
+    #         [(1, 5)],
+    #         [(2, 3), (2, 4)],
+    #         [(2, 3)],
+    #         [(2, 3), (2, 4)],
+    #         [(3, 5)],
+    #         [(4, 3), (4, 4)],
+    #         [(4, 3)],
+    #         [(4, 3), (4, 4)],
+    #         [(5, 5)],
+    #         [(6, 2), (6, 3), (6, 4)]
+    #     ]
+
+    #     grouped = group_points(points)
+
+    #     self.assertTrue(expected[0] in grouped)
+    #     self.assertTrue(expected[1] in grouped)
+    #     self.assertTrue(expected[3] in grouped)
+    #     self.assertEqual(len(grouped), 12)
+
+    # def test_solution_small_input_05(self):
+    #     grid = read_input("small_input_05.txt")
+    #     points = count_points(grid, "A", 0, 0)
+    #     side = group_points(points)
+
+    #     self.assertEqual(len(side), 12)
+    
+    # def test_solution_small_input_02(self):
+    #     grid = read_input("small_input_02.txt")
+    #     points = count_points(grid, "O", 0, 0)
+    #     side = group_points(points)
+
+    #     self.assertEqual(len(side), 20)
+
+    def test_flood_field(self):
+        grid = read_input("small_input_05.txt")
+        points = flood_field(grid, "A", 0, 0, set())
+
+        self.assertTrue((0, 0) in points)
+        self.assertTrue((1, 3) not in points)
+
+    def test_solition_small_input_05(self):
+        grid = read_input("small_input_05.txt")
+        points = flood_field(grid, "A", 0, 0, set())
+        side = find_corners(grid, points)
+
+        self.assertEqual(side, 12)
+    
+    def test_solition_small_input_02(self):
+        grid = read_input("small_input_02.txt")
+        points = flood_field(grid, "O", 0, 0, set())
+        side = find_corners(grid, points)
+
+        self.assertEqual(side, 20)
